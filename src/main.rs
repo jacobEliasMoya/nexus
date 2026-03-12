b268de262acf031cee9980725c66016b2d56eb59
jacob moya <jemoya1994@Gmail.com>
2026-03-12 01:05:20 -0700

    adding in file structure using some more modern rust approaches

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+// This will serve as the main entry point into the project
+
+// Need to basically find a way to do a few different things
+// 1) I think I should parse through each dir & print out
+
 fn main() {
-    println!(
-        "This is the start of my nexus project, bingo bongo bango lets get it in like a dodge durango"
-    );
+    println!("");
 }
