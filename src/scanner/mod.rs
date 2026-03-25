a4bf3831afa08b13c0f1d811ecd22da5c92ca4dc
jacob moya <jemoya1994@Gmail.com>
2026-03-25 00:26:22 -0700

    minor additions, planning on starting a piece of the writer but of the program to write data into a new md file or something easy for AI digestion - moving so not much time recently, oh life, how I love thy mysteries

--- src/scanner/mod.rs
+++ src/scanner/mod.rs
@@ -2,7 +2,7 @@ use std::fs::read_dir;
 use std::path::Path; // this is just a simple import for the Path
 
 pub fn scan_repo() {
-    println!("Hello, scanning now");
+    println!("Hello, scanning now, ... ");
 }
 
 pub fn infer_directory(path: &Path) {
