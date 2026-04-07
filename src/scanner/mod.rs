e5440b1e9814b14a9e88d54d939c70b47376291f
Jake <jemoya1994@gmail.com>
2026-04-07 00:00:17 -0700

    refactoring for better data handling

--- src/scanner/mod.rs
+++ src/scanner/mod.rs
@@ -5,16 +5,20 @@ pub fn scan_repo() {
     println!("Hello, scanning now, ... ");
 }
 
-pub fn infer_directory(path: &Path) {
+pub fn infer_directory(path: &Path) -> Vec<FileEntry> {
+    let mut files = Vec::new();
     let entries = read_dir(path).unwrap();
     for entry in entries {
         match entry {
             Ok(entry) => {
-                println!("{}", entry.path().display());
+                let path = entry.path();
+                let is_dir = path.is_dir();
+                files.push(FileEntry { path, is_dir });
             }
             Err(err) => {
                 println!("Error reading entry: {:?}", err);
             }
         }
     }
+    files
 }
